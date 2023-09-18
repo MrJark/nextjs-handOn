9639c7f5d4f05a0368f0a182fac7bf0ebdf5ad46
Chema <mrjark7@gmail.com>
2023-09-18 09:17:37 +0200

    added pages

---
+++ devtalles_/01-first-steps/app/contact/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Contact () {
+    return (
+      <>
+          <h1 className=" text-5xl text-red-300">Contact page</h1>
+      </>
+    )
+  }
+  
---
+++ devtalles_/01-first-steps/app/pricing/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Pricing () {
+    return (
+      <>
+          <h1 className=" text-5xl text-red-300">Pricing page</h1>
+      </>
+    )
+  }
+  
