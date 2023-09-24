2099524bc9d17e81cc1ddb52ee84fbc673fa36c0
Chema <mrjark7@gmail.com>
2023-09-24 18:31:01 +0200

    f s

--- midudev_/03-twitter-clon/src/app/page.tsx
+++ midudev_/03-twitter-clon/src/app/page.tsx
@@ -1,6 +1,6 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers' // son necesarias para que supabase sepa el estado del usuario
-import { AuthButton } from './components/AuthButton'
+import { AuthButton } from './components/AuthButtonClient'
 
 export default async function Home () {
   const supabase = createServerComponentClient({ cookies }) // no es la parte del cliente por eso se usan las cookies
