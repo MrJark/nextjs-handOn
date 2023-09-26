13f8b0cc75289c63355264503deb27700b782278
Chema <mrjark7@gmail.com>
2023-09-26 16:59:49 +0200

    finished section

--- devtalles_/03-admin-todo/src/app/api/todos/[id]/route.ts
+++ devtalles_/03-admin-todo/src/app/api/todos/[id]/route.ts
@@ -1,4 +1,5 @@
 import prisma from '@/app/lib/prisma';
+import { Todo } from '@prisma/client';
 import { NextResponse, NextRequest } from 'next/server'
 import * as yup from 'yup';
 
@@ -8,14 +9,25 @@ interface Segments { // estos segmentos aunque pongas number, serán string porq
   }
 }
 
+// para no repetir el código y centralizar
+const getTodo = async ( id: string ): Promise< Todo | null > => {
+  
+  const todo = await prisma.todo.findFirst({ where: { id: id } });
+
+  return todo;
+}
+
 export async function GET(request: Request, segments: Segments) {
   // tb se puede desestructurar los params del segment y de ahí ya coges el id con params.id
+  
   const { params } = segments;
   const { id } = params;
+
   // console.log({segments}); // segments: { params: { id: '7a98f18f-5267-4b4f-a46d-df13db69b3e4' } }
   // *Tarea: hacer que a partir del id salga el TODO ❌ no he sabido traer la ruta del todos y por tanto, no he sabido como conseguir que aparezcan
   
-  const todo = await prisma.todo.findFirst({ where: { id: id } })
+  // const todo = await prisma.todo.findFirst({ where: { id: id } })
+  const todo = await getTodo(id)
   if ( !todo ) {
     return NextResponse.json({ message: `That TODO with id: ${id} is not exist`}, { status: 404 });
   }
@@ -31,11 +43,12 @@ const putSchema = yup.object({
 export async function PUT(request: Request, segments: Segments) {
   // tb se puede desestructurar los params del segment y de ahí ya coges el id con params.id
   const { params } = segments;
-  const { id } = params;
+  const { id } = params; 
   // console.log({segments}); // segments: { params: { id: '7a98f18f-5267-4b4f-a46d-df13db69b3e4' } }
   // *Tarea: hacer que a partir del id salga el TODO ❌ no he sabido traer la ruta del todos y por tanto, no he sabido como conseguir que aparezcan
   
-  const todo = await prisma.todo.findFirst({ where: { id: id } })
+  // const todo = await prisma.todo.findFirst({ where: { id: id } }) // por el getTodo
+  const todo = await getTodo(id)
   if ( !todo ) {
     return NextResponse.json({ message: `That TODO with id: ${id} is not exist`}, { status: 404 });
   }
