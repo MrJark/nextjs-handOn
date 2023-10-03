16248a4aa83766598e0ed2d0a5ead7e4bfd4bf42
Chema <mrjark7@gmail.com>
2023-10-03 15:35:06 +0200

    added logIn and logOut functionality

--- devtalles_/03-admin-todo/src/components/LogoutBtn.tsx
+++ devtalles_/03-admin-todo/src/components/LogoutBtn.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {useSession} from "next-auth/react"; // la ruta puede cambiar
+import {useSession, signIn, signOut} from "next-auth/react"; // la ruta puede cambiar
 import {CiLogout} from "react-icons/ci";
 import {BiLoader} from "react-icons/bi";
 
@@ -17,8 +17,21 @@ export const LogoutBtn = () => {
     );
   }
 
+  if (status === "unauthenticated") {
+    return (
+      <button
+        onClick={() => signIn()} // para hacerl logIn de la parte de next-auth
+        className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
+        <CiLogout />
+        <span className="group-hover:text-gray-700">Witing...</span>
+      </button>
+    );
+  }
+
   return (
-    <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
+    <button
+      onClick={() => signOut()} // para hacerl logOut de la parte de next-auth
+      className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
       <CiLogout />
       <span className="group-hover:text-gray-700">Logout</span>
     </button>
