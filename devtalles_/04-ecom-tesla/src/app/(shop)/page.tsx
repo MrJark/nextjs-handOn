f911f00a945b8244182ffc48b5bcb336f3c38170
Chema <mrjark7@gmail.com>
2023-11-27 17:08:55 +0100

    adding routes

--- devtalles_/04-ecom-tesla/src/app/(shop)/page.tsx
+++ devtalles_/04-ecom-tesla/src/app/(shop)/page.tsx
@@ -1,11 +1,11 @@
-import { titleFont } from "./config/fonts"
+import { titleFont } from "../../config/fonts"
 
 
 export default function Home() {
   return (
-    <main className="">
+    <div className="">
       <h1 className='text-2xl'>Hello Word</h1>
       <h2 className={ `${titleFont.className} font-bold text-3xl text-red-300`}>Hola Mundo</h2>
-    </main>
+    </div>
   )
 }
---
+++ devtalles_/04-ecom-tesla/src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import { titleFont } from "../../../config/fonts"
+
+
+export default function Auth() {
+  return (
+    <div className="">
+      <h1 className='text-2xl'>Login Page</h1>
+      <h2 className={ `${titleFont.className} font-bold text-3xl text-red-300`}>Hola Mundo</h2>
+    </div>
+  )
+}
