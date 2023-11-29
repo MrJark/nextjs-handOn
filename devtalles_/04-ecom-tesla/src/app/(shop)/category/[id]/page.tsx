63c8e5a5797b8529368393ce80f9e257a129d95e
Chema <mrjark7@gmail.com>
2023-11-29 19:52:36 +0100

    added not-found page

--- devtalles_/04-ecom-tesla/src/app/(shop)/category/[id]/page.tsx
+++ devtalles_/04-ecom-tesla/src/app/(shop)/category/[id]/page.tsx
@@ -1,7 +1,21 @@
-export default function Category_with_Id() {
+import { notFound } from "next/navigation"
+
+interface Props {
+  params: {
+    id: string,
+  }
+}
+
+export default function Category_with_Id({ params }: Props) {
+  const {id} = params
+
+  if (id === 'kids' ){
+    notFound(); // función especial de NEXT para que aparezca la pantalla de NotFound
+  }
   return (
     <div>
       <h1>Category Id Page</h1>
+      <p>{id}</p>
     </div>
   )
 }
