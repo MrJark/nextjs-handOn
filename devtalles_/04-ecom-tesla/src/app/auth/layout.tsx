3106afccb3f0bbeed0c6ab5fb228ba0c714ca721
Chema <mrjark7@gmail.com>
2023-12-07 18:21:23 +0100

    added login and signup

--- devtalles_/04-ecom-tesla/src/app/auth/layout.tsx
+++ devtalles_/04-ecom-tesla/src/app/auth/layout.tsx
@@ -1,12 +1,11 @@
-
 export default function AuthLayout({
- children
+  children,
 }: {
- children: React.ReactNode;
+  children: React.ReactNode;
 }) {
   return (
-    <main className="min-h-screen bg-green-300">
-      {children}
+    <main className="flex justify-center">
+      <div className="w-full sm:w-[350px] px-10">{children}</div>
     </main>
   );
 }
--- devtalles_/04-ecom-tesla/src/app/auth/login/page.tsx
+++ devtalles_/04-ecom-tesla/src/app/auth/login/page.tsx
@@ -1,11 +1,40 @@
-import { titleFont } from "../../../config/fonts"
+// gist de fernando
+import { titleFont } from "@/config/fonts";
+import Link from "next/link";
 
-
-export default function Auth() {
+export default function LogInPage() {
   return (
-    <div className="">
-      <h1 className='text-2xl'>Login Page</h1>
-      <h2 className={ `${titleFont.className} font-bold text-3xl text-red-300`}>Hola Mundo</h2>
-    </div>
-  )
+    <section className="flex flex-col min-h-screen pt-32 sm:pt-52">
+      <header>
+        <h1 className={`${titleFont.className} text-4xl mb-5`}>Log In</h1>
+      </header>
+
+      <article className="flex flex-col">
+        <label htmlFor="email">Email address</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5 text-black font-medium"
+          type="email"
+        />
+
+        <label htmlFor="email">Password</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5 text-black font-medium"
+          type="password"
+        />
+
+        <button className="btn-primary">Log In</button>
+
+        {/* divisor l ine */}
+        <div className="flex items-center my-5">
+          <div className="flex-1 border-t border-gray-500"></div>
+          <div className="px-2 text-gray-800">O</div>
+          <div className="flex-1 border-t border-gray-500"></div>
+        </div>
+
+        <Link href="/auth/new-account" className="btn-secondary text-center">
+          Create new account
+        </Link>
+      </article>
+    </section>
+  );
 }
