bea24e6496065dc87354fe2b5bf51451324020c9
Chema <mrjark7@gmail.com>
2023-12-07 17:30:35 +0100

    added empty cart page

--- devtalles_/04-ecom-tesla/src/app/(shop)/cart/page.tsx
+++ devtalles_/04-ecom-tesla/src/app/(shop)/cart/page.tsx
@@ -2,6 +2,7 @@ import { QuantitySelector, Title } from "@/components";
 import { initialData } from "@/seed/seed";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 // como datos ficticios ya que esto será por las cookies
 const produtsInCart = [
@@ -10,22 +11,23 @@ const produtsInCart = [
   initialData.products[2],
 ];
 export default function Cart() {
+  // redirect("/empty");
   return (
     <main className="flex justify-center items-center mb-72">
       <div className="flex flex-col w-[1000px]">
         <Title title="Your cart" />
         <div className="flex flex-row justify-between items-center m-5">
-          <span className="text-3xl ">Items</span>
+          <h2 className="text-3xl ">Items</h2>
           <Link href="/" className="mb-5 underline">
             Continue shopping
           </Link>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
           {/* Cart */}
-          <section className="flex flex-col m-5">
+          <div className="flex flex-col m-5">
             {/* Items */}
             {produtsInCart.map((product) => (
-              <div key={product.slug} className="flex pb-8">
+              <section key={product.slug} className="flex pb-8">
                 <Image
                   src={`/products/${product.images[0]}`}
                   alt={product.title}
@@ -37,15 +39,15 @@ export default function Cart() {
                   height={100}
                   className="mr-5 rounded-lg"
                 />
-                <div>
+                <section>
                   <p>{product.title}</p>
                   <p>${product.price}</p>
                   <QuantitySelector className="mt-2" quantity={1} />
                   <button className="underline mt-3">Remove</button>
-                </div>
-              </div>
+                </section>
+              </section>
             ))}
-          </section>
+          </div>
           {/* Summary */}
           <section className="h-fit bg-neutral-700 rounded-xl shadow-xl shadow-neutral-900 p-3 border-none mx-4 sm:mx-0 sm:p-6">
             <h2 className="text-2xl mb-2">Order summary</h2>
