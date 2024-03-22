118836bc394b1c40dfeff935cacda68a68a273cd
Chema <mrjark7@gmail.com>
2024-03-22 21:16:03 +0100

    added pages and common layout for all sub-pages

---
+++ devtalles_course/01-first_app/app/(public)/layout.tsx
@@ -0,0 +1,15 @@
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="flex flex-col items-center p-24">
+      <h1 className="text-pink-500">
+        Hello to <span className="italic font-bold">The Growth Boy</span>{" "}
+        Podcast
+      </h1>
+      {children}
+    </main>
+  );
+}
