2509d89ba2662ace3432fd5d7e46cab163530669
Chema <mrjark7@gmail.com>
2024-04-05 17:35:19 +0200

    added no-found pokemon page

---
+++ devtalles_course/02-dashboard/src/app/dashboard/pokemon/pokedex/[name]/not-found.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="p-0 h-full w-full flex flex-col justify-center items-center bg-[#1A2238]">
+      <h1 className="text-9xl font-extrabold text-white tracking-widest">
+        404
+      </h1>
+      <div className="bg-[#ff3ea5] px-2 text-lg rounded rotate-12 absolute">
+        Pokemon Not Found
+      </div>
+      <button className="mt-5">
+        <Link
+          href="/dashboard/pokemon"
+          className="relative inline-block text-sm font-medium text-[#ff3ea5] group active:text-orange-500 focus:outline-none focus:ring"
+        >
+          <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#ff3ea5] group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+          <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
+            Go Pokedex
+          </span>
+        </Link>
+      </button>
+    </main>
+  );
+}
