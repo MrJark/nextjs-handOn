1b27170073382ee228c85337596730f2e603c85d
Chema <mrjark7@gmail.com>
2024-04-06 14:45:00 +0200

    added useAppDispatch and useAppSelector

--- devtalles_course/02-dashboard/src/store/index.ts
+++ devtalles_course/02-dashboard/src/store/index.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from "./counter/counterSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
   reducer: {
@@ -12,3 +13,7 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>(); // sustituye al useDispatch: se comunica con el store y hace el dispatch de acciones en el store
+export const useAppSelector = useSelector.withTypes<RootState>(); // sustituye al useSelector: sirve para escuchar ciertas partes del state y que cuando cambie, se redibujen
